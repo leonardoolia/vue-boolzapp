6ebf12212f38c2df2dca6405e1b08c622cf3100c
leonardoolia <leonardooliaa@gmail.com>
2024-01-27 14:12:04 +0100

    fixed layout and added delete message function

--- js/script.js
+++ js/script.js
@@ -273,6 +273,18 @@ const app = createApp({
             }, 1000);
         },
 
+        deleteMessage(messageId) {
+            const messageIndex = this.currentChat.findIndex(message => message.id === messageId);
+
+            // Check if there's only one message left
+            if (this.currentChat.length === 1) {
+                // Delete the message
+                this.currentChat.splice(0, 1);
+            } else if (messageIndex !== -1) {
+                // Remove the message from the currentChat array
+                this.currentChat.splice(messageIndex, 1);
+            }
+        }
 
     },
 
